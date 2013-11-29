73291477077aaa48712fdb76fba39c79797fe9b6
Yury Yantsevich <featalion@gmail.com>
2013-11-30 00:36:23 +0100

    adds ids filter for collections routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,22 @@ class ApplicationController < ActionController::API
   protected
 
   def load_paginated(klass, with_user = false)
-    limit = params.fetch(:per_page, 10).to_i
-    offset = params.fetch(:page, 0).to_i * limit
-
     model_set = with_user ? klass.where(user_id: current_user.id) : klass
-    model_set.limit(limit).offset(offset).load
+
+    # Request example: http://api.wyshme.com/api/items?ids=1,7,13
+    # Code below will convert string "1,7,13" to [1, 7, 13],
+    # removes duplications and IDs, which are less than 1.
+    ids = params.fetch(:ids, "").split(',').map(&:to_i)
+    ids.uniq!.delete_if { |id| id < 1 }
+    if ids.empty?
+      limit = params.fetch(:per_page, 10).to_i
+      offset = params.fetch(:page, 0).to_i * limit
+
+      model_set.limit(limit).offset(offset).load
+    else
+      # do not apply pagination if `ids` filter is given
+      model_set.where(id: ids).load
+    end
   end
 
   def set_associations(to, assoc_class, ids)
