081d3c45916d14c77daeca717999f973dd7e7620
Yury Yantsevich <featalion@gmail.com>
2013-12-01 13:48:27 +0100

    allows CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :null_session #:exception
+  before_filter :set_cors_headers
 
   protected
 
@@ -11,8 +12,9 @@ class ApplicationController < ActionController::API
     # Request example: http://api.wyshme.com/api/items?ids=1,7,13
     # Code below will convert string "1,7,13" to [1, 7, 13],
     # removes duplications and IDs, which are less than 1.
-    ids = params.fetch(:ids, "").split(',').map(&:to_i)
-    ids.uniq!.delete_if { |id| id < 1 }
+    ids = params.fetch(:ids, "")
+      .split(',').map(&:to_i)
+      .uniq.delete_if { |id| id < 1 }
     if ids.empty?
       limit = params.fetch(:per_page, 10).to_i
       offset = params.fetch(:page, 0).to_i * limit
@@ -36,6 +38,15 @@ class ApplicationController < ActionController::API
       h[:action] = action_name
       h[:status] = status ? 'success' : 'error'
     end
+  end
+
+  private
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
 end
