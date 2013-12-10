21d3c9a61425dcaff0cc3a5c9334fd0a9994824c
Yury Yantsevich <featalion@gmail.com>
2013-12-10 03:18:39 +0300

    adds uniqueness of name of category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_many :categories_items, dependent: :destroy
   has_many :items, through: :categories_items
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
