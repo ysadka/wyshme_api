32e65acc843861662a59deab7ff81ecd7d323e5b
Yury Yantsevich <featalion@gmail.com>
2013-12-22 15:23:56 +0300

    removes odd callbacks from models

--- app/models/item_like.rb
+++ app/models/item_like.rb
@@ -2,16 +2,7 @@ class ItemLike < ActiveRecord::Base
   belongs_to :user, dependent: :destroy
   belongs_to :item, dependent: :destroy
 
-  before_save :update_counter
-
   validates :user_id, presence: true
   validates :item_id, presence: true
-
-  def update_counter
-    if self.has_attribute? :count
-      self.counter ||= 0
-      self.counter += 1
-    end
-  end
 
 end
