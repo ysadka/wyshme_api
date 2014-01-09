3cdd8248736152992856673c692271f14ec655a9
Yaron <ysadka@gmail.com>
2014-01-09 13:22:15 -0800

    add list of items for response

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,6 +1,6 @@
 class ListSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :event, :event_at,
-             :user_id, :is_deleted, :errors
+             :user_id, :is_deleted, :errors, :list_of_items
   has_many :items
 
   def user_id
@@ -15,4 +15,10 @@ class ListSerializer < ActiveModel::Serializer
     object.errors.map { |f, m| { f => m } }
   end
 
+  def list_of_items
+    items = []
+    object.item_order.split(',').each do |id|
+      items << Item.find_by(id: id.to_i)
+    end
+  end
 end
