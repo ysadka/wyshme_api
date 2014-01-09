8755ea231eff4494ffcbac39356ca548b891cbc4
Yaron <ysadka@gmail.com>
2014-01-09 11:11:46 -0800

    events_lists association

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,8 @@ class List < ActiveRecord::Base
   has_many :items_lists, dependent: :destroy
   has_many :items, through: :items_lists
 
+  has_many :events_lists
+  has_many :events, through: :events_lists
+
   validates :user, :name, presence: true
 end
